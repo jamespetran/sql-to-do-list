54eeffeae2a1b2f5e582b45bec6beae8df51aca7
James Petran <jm.ptrn@gmail.com>
2022-01-06 22:29:34 -0600

    basic client.js complete

--- server/public/script/client.js
+++ server/public/script/client.js
@@ -1,1 +1,51 @@
 console.log('js');
+
+$(onReady);
+
+function onReady() {
+      console.log('JQ');
+      // append HTML
+
+      // event handlers
+
+
+      // GET call
+      todoGet()
+}; // end onReady
+
+// GET 
+function todoGet() {
+      console.log('in GET');
+      
+      let result = {
+            taskname: 'example task',
+            isComplete: false,
+            timeCompleted: null,
+      }
+      render(result);
+}
+
+// POST 
+function todoPost() {
+      console.log('in POST');
+
+}
+// PUT 
+function todoPut() {
+      console.log('in PUT');
+
+}
+
+// DELETE 
+function todoDelete() {
+      console.log('in DELETE');
+
+}
+
+// RENDER TO DOM 
+
+function render(data) {
+      console.log('in render');
+
+
+}
