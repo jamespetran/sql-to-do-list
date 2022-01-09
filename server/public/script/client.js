5a8449202201670e7353cbdf66a699c0d0496aae
James Petran <jm.ptrn@gmail.com>
2022-01-09 14:12:06 -0600

    did POST and 1/2 of PUT

--- server/public/script/client.js
+++ server/public/script/client.js
@@ -4,19 +4,16 @@ $(onReady);
 
 function onReady() {
       console.log('JQ');
-      // append HTML
-
       // event handlers
-
-
-      // GET call
-      todoGet()
+      $('#new-task').on('click', todoPost);
+      $('#taskTable').on('change', '.isNotDone', todoPut);
+      // GET & render init
+      todoGet();
 }; // end onReady
 
 // GET 
 function todoGet() {
       console.log('in GET');
-      
       $.ajax({
             method: 'GET',
             url: '/todo',
@@ -26,53 +23,78 @@ function todoGet() {
       }).catch((err) => {
             console.log('error in GET /todo', err);
       })
-
-}
+} // end todoGet
 
 // POST 
 function todoPost() {
       console.log('in POST');
+      let newTaskInput = prompt("New task name:");
+      console.log('adding new todo:', newTaskInput);
+      let newTaskInputObject = {
+            task: newTaskInput
+      }
 
+      $.ajax({
+            method: 'POST',
+            url: '/todo',
+            data: newTaskInputObject 
+      }).then((res) => {
+            console.log('in POST /todo', res);
+            todoGet(); // GETs and renders todo list
+      }).catch((err) => {
+            console.log('error in POST /todo', err);
+      })
 }
 // PUT 
 function todoPut() {
-      console.log('in PUT');
+      let id = $(this).parent().parent().data('id');
+      console.log('in PUT set complete for', id);
+      $.ajax({
+            method: 'PUT',
+            url: `/todo/edit/${id}`
+      }).then((res) => {
+            console.log(`PUT /todo/edit/${id} complete`, res);
+            todoGet(); // GETs and renders todo list
+      }).catch((err) => {
+            console.log(`error in PUT /todo/edit/${id}`, err);
+      });
 
-}
+} // end todoPut
 
 // DELETE 
 function todoDelete() {
       console.log('in DELETE');
 
-}
+} // end todoDelete
 
 // RENDER TO DOM 
 
 function render(response) {
       console.log('in render');
+      $('#taskTable').empty();
       console.log(response);
       for (const task of response) {
-            let isDone, time, trClass;
-
+            let isDone, time, trClass, rowspan;
             if (task.isDone) {
-                  isDone = 'Complete!';
-                  time = task.whenComplete;
-                  trClass = 'table-success';
+                  isDone = `
+                  <input type="checkbox" 
+                  class="checkbox isDone" 
+                  onclick="this.checked=!this.checked;" 
+                  checked>`;
+                  time = `Completed: ${task.whenComplete}`;
+                  trClass = ' table-success';
+                  rowspan=2;
             } else {
-                  isDone = '';
+                  isDone = `<input type="checkbox" class="checkbox isNotDone">`;
                   time = '';
+                  trClass = '';
+                  rowspan=1;
             }
-            
-
             $('#taskTable').append(`
-            <tr class="tr-stripe ${trClass} " data-id="${task.id}">
-                  <td class="td" data-name="${task.taskname}"> ${task.taskname} </td>
-                  <td class="td" data-bool="${task.isDone}"> ${isDone} </td>
-                  <td class="td" data-timeComplete="${task.whenComplete}"> ${time} </td>
+            <tr class="tr-stripe${trClass} " data-id="${task.id}">
+                  <td class="td taskname" data-name="${task.taskname}"> ${isDone} ${task.taskname} </td>
+                  <td class="td complete fit" data-timeComplete="${task.whenComplete}"> ${time} </td>
             </tr>
             `);
-
       }
-
-
-}
+} // end render
--- server/router/todo.router.js
+++ server/router/todo.router.js
@@ -22,29 +22,28 @@ pool.on('error', (err) => {
 });
 
 // GET
-
-todoRouter.get('/', (req,res) => {
+todoRouter.get('/', (req, res) => {
       console.log('in GET /todo');
 
       let queryText = `
       SELECT id, taskname, "isDone", TO_CHAR("whenComplete", 'HH12:MI AM, Mon fmDDth YYYY') AS "whenComplete" 
       FROM todo
-      ORDER BY "isDone" ASC`;
+      ORDER BY "whenComplete" DESC`;
 
       pool.query(queryText)
-      .then((dbRes) => {
-            res.send(dbRes.rows);
-      })
-      .catch((err) => {
-            console.log('error in GET /todo', err);
-            res.sendStatus(500);
-      })
-});
+            .then((dbRes) => {
+                  console.log("GET /todo 👍")
+                  res.status(200).send(dbRes.rows); // status: ok
+            })
+            .catch((err) => {
+                  console.log('error in GET /todo', err);
+                  res.sendStatus(500); // status: server error
+            })
+}); // end GET /todo
 
 // POST
-
-todoRouter.post('/', (req,res) => {
-      let newTask = req.body;
+todoRouter.post('/', (req, res) => {
+      let newTask = req.body.task;
       console.log('adding task', newTask);
 
       let queryText = `
@@ -53,16 +52,67 @@ todoRouter.post('/', (req,res) => {
             VALUES ($1, false)
       `;
 
-      let queryParams = [ newTask.name ]
+      let queryParams = [newTask]
 
-      if (!testParams(queryParams) ) {
+      if (!testParams(queryParams)) {
             console.log('Data validation Failed ~🤢~');
-            res.sendStatus(400);
+            res.sendStatus(400); // status: data validation fail
             return;
       }
-});
+
+      pool.query(queryText, queryParams)
+            .then(() => {
+                  console.log("POST /todo 👍");
+                  res.sendStatus(201); //status: request successful
+            })
+            .catch((err) => {
+                  console.log("POST /todo 👎", err);
+                  res.sendStatus(500); // status: server error
+            });
+}); // end POST /todo
 
 // PUT
+todoRouter.put('/edit/:id', (req, res) => {
+      console.log(`in PUT /todo/${req.params.id}`);
+      let queryText = `
+            UPDATE "todo"
+            SET   "isDone" = TRUE
+                  "whenComplete" = LOCALTIMESTAMP(0)
+            WHERE id = $1
+      `
+      let queryParams = [req.params.id]
+      
+      // if (opposite(passes test)) === if (fails test):
+      if (!testID(queryParams)) {
+            console.log('Data validation Failed ~🤢~');
+            res.sendStatus(400); // status: data validation fail
+            return;
+      };
+
+      pool.query(queryText, queryParams)
+            .then(() => {
+                  res.sendStatus(201) //status: request successful
+            })
+            .catch((err) => {
+                  console.log(`Error in PUT /todo/edit/${req.params.id}`)
+                  res.sendStatus(500); // status: server error
+            });
+
+
+})
+
+function testID(queryParams) {
+      // regex = numbers 0-9 @ one or many digits
+      let idRX = /^[0-9]+$/; 
+      // if passes test for regex:
+      if (idRX.test(queryParams[0])) {
+            return true
+      } else {
+            return false
+      }
+
+}
+
 
 // DELETE
 
